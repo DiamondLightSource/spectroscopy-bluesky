8b101f5622d652ea89c59bf804346b6fde27ca91
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-04-28 11:17:14 +0100

    change the config server plans

--- src/spectroscopy_bluesky/i18/config_server/server.py
+++ src/spectroscopy_bluesky/i18/config_server/server.py
@@ -1,1 +1,2 @@
 # todo write a fastapi service that caaches the values against a fs backend
+# todo actally following https://github.com/DiamondLightSource/dodal/pull/1186 make this an ophyd-async device
