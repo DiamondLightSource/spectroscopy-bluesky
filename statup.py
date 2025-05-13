fdc37b5ea43527774a0bdc465dee9da00813f640
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-05-13 15:43:47 +0100

    Update bi-directional scans

--- statup.py
+++ statup.py
@@ -1,8 +1,12 @@
 from bluesky import RunEngine
-from dodal.beamlines.i20_1 import turbo_slit
-from spectroscopy_bluesky.i20_1.plans.direct_turbo_slit_movement import fly_scan_ts, panda
+from dodal.beamlines.i20_1 import turbo_slit, panda
+from spectroscopy_bluesky.i20_1.plans.direct_turbo_slit_movement import (
+    fly_scan_ts,
+    fly_sweep,
+    fly_sweep_both_ways,
+)
 from ophyd_async.plan_stubs import ensure_connected
 
 
 RE = RunEngine()
-RE(ensure_connected(turbo_slit(),panda))
+RE(ensure_connected(turbo_slit(), panda()))
