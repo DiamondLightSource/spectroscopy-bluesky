373eb0df9d47691965783d6903921c98dc330efd
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-05-14 16:09:29 +0100

    Add initial version of trajectory scans

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from bluesky import RunEngine
 from dodal.beamlines.i20_1 import turbo_slit, panda
-from spectroscopy_bluesky.i20_1.plans.direct_turbo_slit_movement import fly_scan_ts, fly_sweep, fly_sweep_both_ways
+from spectroscopy_bluesky.i20_1.plans.direct_turbo_slit_movement import trajectory_fly_scan
 from ophyd_async.plan_stubs import ensure_connected
 
 
@@ -9,4 +9,4 @@ p = panda()
 
 RE = RunEngine()
 RE(ensure_connected(t,p))
-RE(fly_sweep_both_ways(start=0, stop=10, num=11, duration=1.0,panda=p, number_of_sweeps=3))
+RE(trajectory_fly_scan(start=0, stop=10, num=11, duration=1.0,panda=p, number_of_sweeps=3))
