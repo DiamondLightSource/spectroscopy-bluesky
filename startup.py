a076a010d34dc331a0adeea10651ba612ccbb57d
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-05-20 09:31:31 +0100

    Formating

--- startup.py
+++ startup.py
@@ -2,12 +2,5 @@ from bluesky import RunEngine
 from dodal.beamlines.i20_1 import panda, turbo_slit
 from ophyd_async.plan_stubs import ensure_connected
 
-from spectroscopy_bluesky.i20_1.plans.direct_turbo_slit_movement import (
-    fly_scan_ts,
-    fly_sweep,
-    fly_sweep_both_ways,
-    panda,
-)
-
 RE = RunEngine()
 RE(ensure_connected(turbo_slit(), panda()))
