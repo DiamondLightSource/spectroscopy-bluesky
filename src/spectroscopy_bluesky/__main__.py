22653ded6da23103c424bce9cfe8dd9edccc6cf1
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-06-19 14:28:01 +0000

    add the docs

--- src/spectroscopy_bluesky/__main__.py
+++ src/spectroscopy_bluesky/__main__.py
@@ -1,35 +1,36 @@
 """Interface for ``python -m spectroscopy_bluesky``."""
 
-from argparse import ArgumentParser
-from collections.abc import Sequence
 
-from . import __version__
+from spectroscopy_bluesky.cli import cli
 
-__all__ = ["main"]
+# from . import __version__
+
+# __all__ = ["main"]
 
 
-def main(args: Sequence[str] | None = None) -> None:
-    """Argument parser for the CLI."""
-    parser = ArgumentParser()
-    parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=__version__,
-    )
-    parser.add_argument(
-        "-g",
-        "--generate",
-    )
-    parser.parse_args(args)
+# def main(args: Sequence[str] | None = None) -> None:
+#     """Argument parser for the CLI."""
+#     parser = ArgumentParser()
+#     parser.add_argument(
+#         "-v",
+#         "--version",
+#         action="version",
+#         version=__version__,
+#     )
+#     parser.add_argument(
+#         "-g",
+#         "--generate",
+#     )
+#     parser.parse_args(args)
 
-    # todo fix this really
-    if args.__getattribute__("generate"):
-        print(
-            "This command is not implemented yet. "
-            "Please use the `spectroscopy_bluesky` package directly."
-        )
+#     # todo fix this really
+#     if args.__getattribute__("generate"):
+#         print(
+#             "This command is not implemented yet. "
+#             "Please use the `spectroscopy_bluesky` package directly."
+#         )
 
 
 if __name__ == "__main__":
-    main()
+    cli()
+    # main()
