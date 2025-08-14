8ac4a68073ff64f5d3948296e410efb5288ad510
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-08-14 12:20:35 +0100

    Added non-linear sequencer table scan

--- src/spectroscopy_bluesky/i20_1/plans/direct_turbo_slit_movement.py
+++ src/spectroscopy_bluesky/i20_1/plans/direct_turbo_slit_movement.py
@@ -316,10 +316,7 @@ def trajectory_fly_scan(
 
     yield from ensure_connected(pmac, motor)
 
-    spec = Fly(
-        float(duration)
-        @ (Repeat(number_of_sweeps, gap=False) * ~Line(motor, start, stop, num))
-    )
+    spec = Fly(float(duration) @ (Line(motor, start, stop, num)))
 
     times = spec.frames().duration
     positions = spec.frames().midpoints[motor]
@@ -449,6 +446,113 @@ def seq_table(
     # Prepare Panda file writer trigger info
     panda_hdf_info = TriggerInfo(
         number_of_events=num,
+        trigger=DetectorTrigger.CONSTANT_GATE,
+        livetime=duration,
+        deadtime=1e-5,
+    )
+
+    @attach_data_session_metadata_decorator()
+    @bpp.run_decorator()
+    @bpp.stage_decorator([panda, panda_seq])
+    def inner_plan():
+        # Prepare pmac with the trajectory
+        yield from bps.prepare(pmac_trajectory_flyer, trigger_logic, wait=True)
+        # prepare sequencer table
+        yield from bps.prepare(panda_seq, seq_table_info, wait=True)
+        # prepare panda and hdf writer once, at start of scan
+        yield from bps.prepare(panda, panda_hdf_info, wait=True)
+
+        # kickoff devices waiting for all of them
+        yield from bps.kickoff(panda, wait=True)
+        yield from bps.kickoff(panda_seq, wait=True)
+        yield from bps.kickoff(pmac_trajectory_flyer, wait=True)
+
+        yield from bps.complete_all(pmac_trajectory_flyer, panda_seq, panda, wait=True)
+
+    yield from inner_plan()
+
+
+def Si111_energies_to_Bragg(Ei, Ef, dE):
+    energies = np.arange(Ei, Ef + dE, dE)  # include Ef as last point in the array
+    angles = np.degrees(np.arcsin(1977.59 / energies))
+    return angles
+
+
+def seq_non_linear(
+    ei: int,
+    ef: int,
+    de: float,
+    duration: float,
+    panda: HDFPanda = inject("panda"),  # noqa: B008)
+):
+    # Defining the frlyers and components of the scan
+    panda_seq = StandardFlyer(StaticSeqTableTriggerLogic(panda.seq[1]))
+    motor = Motor(prefix="BL20J-OP-PCHRO-01:TS:XFINE", name="X")
+    pmac = PmacIO(
+        prefix="BL20J-MO-STEP-06:", raw_motors=[motor], coord_nums=[3], name="pmac"
+    )
+    yield from ensure_connected(pmac, motor)
+    angle = Si111_energies_to_Bragg(ei, ef, de)
+    energies = np.arange(ei, ef + de, de)
+    print(f"\n\nlen(angle) = {len(angle)}\n\n")
+
+    table = SeqTable()  # type: ignore
+
+    # Prepare motor info using trajectory scanning
+    spec = Fly(
+        float(duration) @ (Line(motor, angle[0], angle[len(angle) - 1], len(angle)))
+    )
+    times = spec.frames().duration if spec.frames().duration is not None else []
+    positions = spec.frames().lower[motor]
+    positions = [int(x / MRES) for x in positions]
+
+    # Writes down the desired positions that will be written to the sequencer table
+    f = h5py.File(
+        f"{PATH}i20-1-extra-{datetime.now().strftime('%Y-%m-%d-%H:%M:%S')}.h5", "w"
+    )
+    f.create_dataset("time", shape=(1, len(times)), data=times)
+    f.create_dataset("positions", shape=(1, len(positions)), data=positions)
+    f.create_dataset("angle", shape=(1, len(angle)), data=angle)
+    f.create_dataset("energies", shape=(1, len(energies)), data=energies)
+    counter = 0
+    angle_int = [int(x / MRES) for x in angle]
+
+    direction = SeqTrigger.POSA_LT
+    if angle_int[0] < angle_int[-1]:
+        direction = SeqTrigger.POSA_GT
+
+    for _t, p in zip(times, angle_int, strict=False):
+        # As we do multiple swipes it's necessary to change the comparison
+        # for triggering the sequencer table.
+        # This is not the best way of doing it but will sufice for now
+        if counter == len(angle):
+            if direction == SeqTrigger.POSA_GT:
+                direction = SeqTrigger.POSA_LT
+            else:
+                direction = SeqTrigger.POSA_GT
+            counter = 0
+
+        table += SeqTable.row(
+            repeats=1,
+            trigger=direction,
+            position=p,
+            time1=1,  # int(0.001 / 1e-6),
+            outa1=True,
+            time2=1,
+            outa2=False,
+        )
+
+        counter += 1
+
+    seq_table_info = SeqTableInfo(sequence_table=table, repeats=1, prescale_as_us=1)
+
+    trigger_logic = PmacTriggerLogic(spec=spec)
+    pmac_trajectory = PmacTrajectoryTriggerLogic(pmac)
+    pmac_trajectory_flyer = StandardFlyer(pmac_trajectory)
+
+    # Prepare Panda file writer trigger info
+    panda_hdf_info = TriggerInfo(
+        number_of_events=len(angle),
         trigger=DetectorTrigger.CONSTANT_GATE,
         livetime=duration,
         deadtime=1e-5,
